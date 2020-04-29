3724d193f78c438f3ab0683168f2cc815df782fa
nasha2878 <namratha1@gmail.com>
2020-04-28 21:11:37 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,50 @@
+using System;
 using Amazon;
 using Amazon.S3;
 using Amazon.S3.Transfer;
 using System.IO;
 using System.Threading.Tasks;
 
-private static IAmazonS3 s3Client;
 
-static void Main(string[] args)
+namespace MyS3
 {
-s3Client = new AmazonS3Client(RegionEndpoint.USEast1);
-var bucketName = args[0];
+    class Program
+    {
+        private static IAmazonS3 s3Client;
+        private const string filePath = "/home/ubuntu/environment/README.md";
+
+        static void Main(string[] args)
+        {
+           s3Client = new AmazonS3Client(RegionEndpoint.USEast1);
+            var bucketName = args[0];
+            
+            //Create Bucket
+            s3Client.PutBucketAsync(bucketName).Wait();
+            Console.WriteLine("Created the bucket named '{0}'.", bucketName);
+            
+             // List available buckets
+              var response = s3Client.ListBucketsAsync().Result;
+        
+              foreach (var bucket in response.Buckets)
+              {
+                Console.WriteLine(bucket.BucketName);
+              }
+           //   Delete bucket
+              s3Client.DeleteBucketAsync(bucketName).Wait();
+              Console.WriteLine("Deleted the bucket named '{0}'.", bucketName);
+              
+           // Upload a file
+	       UploadFileAsync().Wait();
+
+
+        }
+        
+         private static async Task UploadFileAsync()
+        {
+            var fileTransferUtility = new TransferUtility(s3Client);    
+            await fileTransferUtility.UploadAsync(filePath, "nam-test-bucket1001");
+            Console.WriteLine("File upload completed");
+        }
+
+    }
 }
